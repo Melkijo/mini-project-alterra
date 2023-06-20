93f8fcac641c476142f31b9e1b47427f741ddb6c
Melkijo <jonathanandaraa@gmail.com>
2023-06-20 17:59:38 +0800

    fix

--- mini-project/src/main.jsx
+++ mini-project/src/main.jsx
@@ -25,131 +25,131 @@ import AdminUsers from "./pages/AdminUsers.jsx";
 import AdminEditPage from "./pages/AdminEditPage.jsx";
 import("preline");
 const wsLink = new GraphQLWsLink(
-  createClient({
-    url: "wss://evolved-akita-43.hasura.app/v1/graphql",
-    connectionParams: {
-      headers: {
-        "x-hasura-admin-secret": import.meta.env.VITE_HASURA_ADMIN_SECRET,
+   createClient({
+      url: "wss://evolved-akita-43.hasura.app/v1/graphql",
+      connectionParams: {
+         headers: {
+            "x-hasura-admin-secret": import.meta.env.VITE_HASURA_ADMIN_SECRET,
+         },
       },
-    },
-  })
+   })
 );
 const httpLink = new HttpLink({
-  uri: "https://evolved-akita-43.hasura.app/v1/graphql",
-  headers: {
-    "x-hasura-admin-secret": import.meta.env.VITE_HASURA_ADMIN_SECRET,
-  },
+   uri: "https://evolved-akita-43.hasura.app/v1/graphql",
+   headers: {
+      "x-hasura-admin-secret": import.meta.env.VITE_HASURA_ADMIN_SECRET,
+   },
 });
 
 const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
-  },
-  wsLink,
-  httpLink
+   ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+         definition.kind === "OperationDefinition" &&
+         definition.operation === "subscription"
+      );
+   },
+   wsLink,
+   httpLink
 );
 
 const client = new ApolloClient({
-  link: splitLink,
-  cache: new InMemoryCache(),
+   link: splitLink,
+   cache: new InMemoryCache(),
 });
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
+   {
+      path: "/",
+      element: <Root />,
 
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: "true",
-            element: <LandingPage />,
-          },
-          {
-            path: "beasiswa",
-            element: <BeasiswaPage />,
-          },
-          {
-            path: "tentang",
-            element: <AboutPage />,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: "/masuk",
-    element: <LoginPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/daftar",
-    element: <RegisterPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/userPage/",
-    element: <User />,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true,
-            element: <UserBeasiswa />,
-          },
-          {
-            path: "detail",
-            element: <UserDetail />,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: "/adminPage",
-    element: <Admin />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        // errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <AdminBeasiswa /> },
-          {
-            path: "upload",
-            element: <AdminUpload />,
-          },
-          {
-            path: "edit/:id",
-            element: <AdminEditPage />,
-          },
-          {
-            path: "userList",
-            element: <AdminUsers />,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: "/beasiswa/:id",
-    element: <BeasiswaDetail />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "*",
-    element: <ErrorPage />,
-  },
+      children: [
+         {
+            errorElement: <ErrorPage />,
+            children: [
+               {
+                  index: "true",
+                  element: <LandingPage />,
+               },
+               {
+                  path: "beasiswa",
+                  element: <BeasiswaPage />,
+               },
+               {
+                  path: "tentang",
+                  element: <AboutPage />,
+               },
+            ],
+         },
+      ],
+   },
+   {
+      path: "/masuk",
+      element: <LoginPage />,
+      errorElement: <ErrorPage />,
+   },
+   {
+      path: "/daftar",
+      element: <RegisterPage />,
+      errorElement: <ErrorPage />,
+   },
+   {
+      path: "/userPage/",
+      element: <User />,
+      children: [
+         {
+            errorElement: <ErrorPage />,
+            children: [
+               {
+                  index: true,
+                  element: <UserBeasiswa />,
+               },
+               {
+                  path: "detail",
+                  element: <UserDetail />,
+               },
+            ],
+         },
+      ],
+   },
+   {
+      path: "/adminPage",
+      element: <Admin />,
+      errorElement: <ErrorPage />,
+      children: [
+         {
+            // errorElement: <ErrorPage />,
+            children: [
+               { index: true, element: <AdminBeasiswa /> },
+               {
+                  path: "upload",
+                  element: <AdminUpload />,
+               },
+               {
+                  path: "edit/:id",
+                  element: <AdminEditPage />,
+               },
+               {
+                  path: "userList",
+                  element: <AdminUsers />,
+               },
+            ],
+         },
+      ],
+   },
+   {
+      path: "/beasiswa/:id",
+      element: <BeasiswaDetail />,
+      errorElement: <ErrorPage />,
+   },
+   {
+      path: "*",
+      element: <ErrorPage />,
+   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ApolloProvider client={client}>
-    {/* <React.StrictMode> */}
-    <RouterProvider router={router} />
-    {/* </React.StrictMode> */}
-  </ApolloProvider>
+   <ApolloProvider client={client}>
+      {/* <React.StrictMode> */}
+      <RouterProvider router={router} />
+      {/* </React.StrictMode> */}
+   </ApolloProvider>
 );
--- mini-project/src/routes/User.jsx
+++ mini-project/src/routes/User.jsx
@@ -4,18 +4,18 @@ import { useAtom } from "jotai";
 import { authAtom } from "../components/Atoms";
 
 export default function User() {
-  const navigate = useNavigate();
-  const [userx] = useAtom(authAtom);
-  if (userx.token === import.meta.env.VITE_ADMIN_COOK) {
-    navigate("/");
-  } else if (userx.token === undefined) {
-    navigate("/");
-  }
-  return (
-    <>
-      <div>
-        <UserSidebar />
-      </div>
-    </>
-  );
+   const navigate = useNavigate();
+   const [userx] = useAtom(authAtom);
+   if (userx.token === import.meta.env.VITE_ADMIN_COOK) {
+      navigate("/");
+   } else if (userx.token === undefined) {
+      navigate("/");
+   }
+   return (
+      <>
+         <div>
+            <UserSidebar />
+         </div>
+      </>
+   );
 }
